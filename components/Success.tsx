ffaba88ca65a88c30069f94cccdba5aa883eb4de
MUKESH B <mukesh@MUKESHs-MacBook-Air.local>
2026-01-23 18:43:00 +0530

    Move submission to useEffect to prevent React re-render duplicates

--- components/Success.tsx
+++ components/Success.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { UserData } from '../types';
 import { useApp } from '../context/AppContext';
 import { Poster } from './Poster';
